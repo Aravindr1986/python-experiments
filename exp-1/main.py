5ce1c610a487b0bdb05ab065eda1cb3d22bde6ca
Aravind R <aravind.ramachandran1@ey.com>
2024-02-05 15:06:05 -0500

    Added 23d arrays

--- exp-1/main.py
+++ exp-1/main.py
@@ -1,4 +1,7 @@
-import numpy
-arr = numpy.array([1, 2, 3, 4, 5])
-
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5]) # 1D array
+arr2D = np.array([[1, 2, 3], [4, 5, 6]])
+arr3D = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 print(arr)
+print(arr2D)
+print(arr3D)
